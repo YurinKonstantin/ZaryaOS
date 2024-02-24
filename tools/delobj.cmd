acba2cb83f92e71d5552701b10b5a00f593d0dcc
yurinkonstantin <yurinkonstantin@mail.ru>
2024-02-24 16:35:17 +0300

    delay

--- tools/delobj.cmd
+++ tools/delobj.cmd
@@ -6,4 +6,4 @@ for /r %%i IN (obj) DO if exist %%i\nul echo :  %%i & rmdir /q /s %%i
 echo *
 echo * Deleting all build.xxx files %cd%
 echo *
-del /f /s build.wrn build.err *~
+del /f /s build.log build.wrn build.err *~
--- tools/delobjProb.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-echo *
-echo * Deleting all OBJs below %cd%
-echo *
-echo *
-for /f "tokens=*" %%i IN (D:\srv03rtm\list.txt) DO echo :  %%i
-echo * Deleting all build.xxx files %cd%
-echo *
-
